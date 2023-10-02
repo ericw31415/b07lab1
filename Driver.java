0244c40b966ea3e79d088143fcdbbe1126ebb722
Eric Wan <ericwan957@gmail.com>
2023-10-01 23:46:15 -0400

    Edit Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,31 @@
+import java.io.File;
+
 public class Driver {
     public static void main(String[] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double[] c1 = {6, 0, 0, 5};
-        Polynomial p1 = new Polynomial(c1);
-        double[] c2 = {0, -2, 0, 0, -9};
-        Polynomial p2 = new Polynomial(c2);
+        double[] c1 = {6, 5};
+        int[] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double[] c2 = {-9, -2};
+        int[] e2 = {4, 1};
+        Polynomial p2 = new Polynomial(c2, e2);
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
         if (s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        Polynomial prod = p1.multiply(p2);
+        System.out.println("prod(0.2) = " + prod.evaluate(0.2));
+        try {
+            Polynomial f = new Polynomial(new File("input.txt"));
+            System.out.println("f(1.9) = " + f.evaluate(1.9));
+            f.saveToFile("output.txt");
+        } catch (Exception e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,30 +1,126 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Polynomial {
     double[] coeffs_;
+    int[] exps_;
 
     public Polynomial() {
-        coeffs_ = new double[]{0};
+        coeffs_ = new double[]{};
+        exps_ = new int[]{};
     }
 
-    public Polynomial(double[] coeffs) {
+    public Polynomial(double[] coeffs, int[] exps) {
         coeffs_ = coeffs;
+        exps_ = exps;
+    }
+
+    public Polynomial(File file) throws Exception {
+        this();
+        try {
+            Scanner reader = new Scanner(file);
+            String[] tokens = reader.nextLine().replaceAll("-", "+-")
+                .split("\\+");
+            reader.close();
+
+            for (String token : tokens) {
+                String term[] = token.split("x");
+                double coeff;
+                int exp;
+                if (Pattern.matches("x.*", token))
+                    coeff = 1.;
+                else if (Pattern.matches("^-x.*", token))
+                    coeff = -1.;
+                else
+                    coeff = Double.parseDouble(term[0]);
+                if (Pattern.matches("[^x]+", token))
+                    exp = 0;
+                else if (Pattern.matches(".*x", token))
+                    exp = 1;
+                else
+                    exp = Integer.parseInt(term[1]);
+                addTerm(coeff, exp);
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void addTerm(double coeff, int exp) {
+        int numCoeffs = coeffs_.length;
+
+        // Check if exponent exists already
+        for (int i = 0; i < numCoeffs; i++) {
+            if (exps_[i] == exp) {
+                coeffs_[i] += coeff;
+                return;
+            }
+        }
+
+        double[] newCoeffs = new double[numCoeffs + 1];
+        int[] newExps = new int[numCoeffs + 1];
+
+        for (int i = 0; i < numCoeffs; i++) {
+            newCoeffs[i] = coeffs_[i];
+            newExps[i] = exps_[i];
+        }
+        newCoeffs[numCoeffs] = coeff;
+        newExps[numCoeffs] = exp;
+        coeffs_ = newCoeffs;
+        exps_ = newExps;
     }
 
     public Polynomial add(Polynomial addend) {
-        int maxLen = Math.max(coeffs_.length, addend.coeffs_.length);
-        double[] coeffs = new double[maxLen];
-        for (int i = 0; i < coeffs_.length; i++)
-            coeffs[i] += coeffs_[i];
+        int numCoeffs = coeffs_.length;
+        Polynomial res = new Polynomial(new double[numCoeffs],
+            new int[numCoeffs]);
+        for (int i = 0; i < numCoeffs; i++) {
+            res.coeffs_[i] = coeffs_[i];
+            res.exps_[i] = exps_[i];
+        }
         for (int i = 0; i < addend.coeffs_.length; i++)
-            coeffs[i] += addend.coeffs_[i];
+            res.addTerm(addend.coeffs_[i], addend.exps_[i]);
 
-        return new Polynomial(coeffs);
+        return res;
+    }
+
+    public Polynomial multiply(Polynomial mult) {
+        Polynomial res = new Polynomial();
+        for (int i = 0; i < coeffs_.length; i++) {
+            for (int j = 0; j < mult.coeffs_.length; j++) {
+                res.addTerm(coeffs_[i] * mult.coeffs_[j],
+                    exps_[i] + mult.exps_[j]);
+            }
+        }
+        return res;
     }
 
     public double evaluate(double x) {
         double res = 0.;
-        for (int i = coeffs_.length - 1; i >= 0; i--)
-            res = res * x + coeffs_[i];
+        for (int i = 0; i < coeffs_.length; i++)
+            res += coeffs_[i] * Math.pow(x, exps_[i]);
         return res;
+    }
+
+    public void saveToFile(String name) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter(name);
+        for (int i = 0; i < coeffs_.length; i++) {
+            if (i > 0 && coeffs_[i] >= 0.)
+                writer.print('+');
+            if (coeffs_[i] == -1.)
+                writer.print('-');
+            if (coeffs_[i] != 1. && coeffs_[i] != -1.)
+                writer.print(coeffs_[i]);
+            if (exps_[i] > 0)
+                writer.print('x');
+            if (exps_[i] > 1)
+                writer.print(exps_[i]);
+        }
+        writer.print('\n');
+        writer.close();
     }
 
     public boolean hasRoot(double x) {
